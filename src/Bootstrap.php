db5aa2ed25eda2e2077769082d202c1d07ab4b7e
John Martin <john.itvn@gmail.com>
2015-07-16 03:32:56 +0700

    Initializing bootstrap for extension

---
+++ src/Bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+namespace johnitvn\rbacplus;
+
+use Yii;
+use yii\base\BootstrapInterface;
+
+/**
+* Hook with application bootstrap stage
+* @author John Martin <john.itvn@gmail.com>
+* @since 1.0.0
+*/
+class Bootstrap implements BootstrapInterface
+{
+    
+    /**
+    * Initial application compoments and modules need for extension
+    * @param \yii\base\Application $app The application currently running
+    * @return void
+    */
+    public function bootstrap($app){  
+
+        // Set alias for extension source
+        Yii::setAlias("@rbacplus","@vendor/johnitvn/yii2-rbac-plus/src");
+
+        // Setup i18n compoment for translate all category user*
+        if (!isset(Yii::$app->get('i18n')->translations['rbac*'])) {
+            Yii::$app->get('i18n')->translations['rbac*'] = [
+                'class'    => 'yii\i18n\PhpMessageSource',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+            
+    }
+
+   
+}
