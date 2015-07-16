f7dfe621b85ec44c1a2fe2a9ca0e527824fd25a8
John Martin <john.itvn@gmail.com>
2015-07-16 19:14:47 +0700

    Add roles manager feature Task #2 - Create role manager feature

---
+++ src/views/role/create.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model johnitvn\rbacplus\models\AuthItem */
+
+?>
+<div class="auth-item-create">
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+</div>
---
+++ src/views/role/view.php
@@ -0,0 +1,10 @@
+<div class="permistion-item-view">
+    <table class="table table-striped table-bordered detail-view">
+        <tbody>
+            <tr><th>Permission Name</th><td><?= $model->name ?></td></tr>
+            <tr><th>Description</th><td><?= $model->description ?></td></tr>
+            <tr><th>Rule Name</th><td><?= $model->ruleName ?></td></tr>
+            <tr><th>Data</th><td><?= $model->data ?></td></tr>                      
+        </tbody>
+    </table>
+</div>
