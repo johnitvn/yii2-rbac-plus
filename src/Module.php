460001604ef947eae22e313ddaafab92cd779e8f
John Martin <john.itvn@gmail.com>
2015-07-23 16:42:45 +0700

    Add `beforeAction` config

--- src/Module.php
+++ src/Module.php
@@ -12,8 +12,7 @@ use yii\base\Module as BaseModule;
  * @since 1.0.0
  */
 class Module extends BaseModule {
-    
-    
+
     /**
      *
      * @var string $userModelClassName The user model class.
@@ -34,14 +33,13 @@ class Module extends BaseModule {
      * Default is username
      */
     public $userModelLoginField = 'username';
-    
-    
+
     /**
      *
      * @var string $userModelLoginFieldLabel The login field's label of user model.
      * Default is Username  
      */
-    public $userModelLoginFieldLabel = 'Username';
+    public $userModelLoginFieldLabel;
 
     /**
      *
@@ -51,15 +49,48 @@ class Module extends BaseModule {
     public $userModelExtraDataColumls;
 
     /**
+     * Callback before create controller
+     * @var mixed 
+     */
+    public $beforeCreateController = null;
+
+    /**
+     * Callback before create action
+     * @var type 
+     */
+    public $beforeAction = null;
+
+    /**
      * Initilation module
      * @return void
      */
     public function init() {
         parent::init();
         if ($this->userModelClassName == null) {
-            $this->userModelClassName = Yii::$app->getUser()->identityClass;
+            if (Yii::$app->has('user')) {
+                $this->userModelClassName = Yii::$app->user->identityClass;
+            } else {
+                throw new yii\base\Exception("You must config user compoment both console and web config");
+            }
         }
-        $this->userModelLoginFieldLabel = Yii::t('rbac', $this->userModelLoginFieldLabel);
+        if ($this->userModelLoginFieldLabel == null) {
+            $model = new $this->userModelClassName;
+            $this->userModelLoginFieldLabel = $model->getAttributeLabel($this->userModelLoginField);
+        }
+    }
+
+    public function createController($route) {
+        if ($this->beforeCreateController !== null && !call_user_func($this->beforeCreateController, $route)) {
+            return false;
+        }
+        return parent::createController($route);
+    }
+
+    public function beforeAction($action) {
+        if ($this->beforeAction !== null && !call_user_func($this->beforeAction, $action)) {
+            return false;
+        }
+        return parent::beforeAction($action);
     }
 
 }
