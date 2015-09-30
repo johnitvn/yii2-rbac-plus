4427d2a359737c28337c3cab0965a98aac9bfebb
Евгений <89125230101@mail.ru>
2015-09-30 10:42:58 +0500

    Translate for ru-RU

---
+++ src/messages/ru-RU/rbac.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    /* =========== Controller common  =============*/
+    'Create new {0}' => 'Создано новых {0}',  
+    'Have been create new {0} success' => 'Успешно создано {0} ', 
+    'Update {0}' => 'Обновлено {0}',  
+    'Have been update {0} success' => 'Были успешно обновлены {0}', 
+    'The requested page does not exist.'=>'Запрашиваемая страница не существует',
+   
+    /* =========== Views common  =============*/
+    /* 1. GridView */
+    '* Resize table columns just like a spreadsheet by dragging the column edges.' => '* Изменение размера столбцов таблицы так же, как таблицы путем перетаскивания края столбцов.', // for toolbar buttons
+    'Reload Grid' => 'Обновить таблицу', // for toolbar buttons    
+    'All' => 'Все', // for toolbar buttons
+    'Show all data' => 'Показать всё', // for toolbar buttons
+    'Page' => 'Страница', // for toolbar buttons
+    'Show first page data' => 'Показать первую страницу', // for action buttons
+    'View'=>'Показать',
+    'Update' => 'Обновить', // for action buttons
+    'Delete' => 'Удалить', // for action buttons    
+    /*2. Modal*/
+    'Close'=>'Закрыть',
+    'Save'=>'Сохранить',
+    'Edit'=>'Редактировать',
+    'Ok'=>'ОК',
+    'Cancel'=>'Отмена',
+    'Create More'=>'Создать ещё',
+    'Are you sure?'=>'Вы уверены?',// for confirm delete
+    'Are you sure want to delete this item'=>'Вы уверены что хотите удалить этот элемент?',// for confirm delete
+    '(not use)'=>'(не используется)',
+  
+    /* =========== Auth Item Model  =============*/
+    'Name'=>'Имя',
+    'Description'=>'Описание',
+    'Rule Name'=>'Имя правила',
+    'Data'=>'Данные',
+    'Rule not exists'=>'Правило не существует',
+    
+    
+       /* =========== Rule Manager  =============*/
+    /* 1. Rule Model */
+    'Class Name'=>'Имя класса',// Rule Model   
+    'Class "{className}" not exist'=>'Класс "{className}" не существует',
+    'Class "{className}" must extends yii\rbac\Rule'=>'Класс "{className}" должен быть расширением yii\rbac\Rule',
+    'The "{className}::\$name" is not set'=>'"{className}::\$name" пустое или не установлено',
+    'The "{className}::\$name" is incorrect with the name of rule you have set'=>'"{className}::\$name" не совпадает с Вашим',
+  
+    
+     /* 2. Rule Views */  
+    'Rules Manager' => 'Менеджер правил', 
+       
+    
+    /* =========== Permisstion Manager  =============*/
+    /* 1. Permisstion Model */
+    'Permission name'=>'Имя разрешения',
+    
+    /* 2. Permisstion Views */  
+    'Permisstions Manager'=>'Менеджер разрешений',
+    
+    
+    /* =========== Permisstion Manager  =============*/
+    /* 1. Permisstion Model */
+    'Role name'=>'Имя роли',
+    
+    /* 2. Permisstion Views */  
+    'Roles Manager'=>'Менеджер ролей',
+
+    /* =========== User role assignemnt  =============*/
+    /* 1. Permisstion Views */      
+    'User Assignment'=>'Соответсвия пользователя',
+    'Assignment'=>'Соответсвия',
+];
+
